d837e3a1e0a808ee08759bf7f0a5adf062fca691
Пользователь <edigaryanmane12@gmail.com>
2024-09-13 16:08:33 +0400

    qwsa

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    Console.WriteLine("qqq");
+            //}
+            //using (StreamReader sw = new StreamReader(path))
+            //{
+            //   string str = sw.ReadLine();
+            //    Console.WriteLine(str);
+            //}
+
+            //2
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //File.Delete(path);
+
+            //Console.WriteLine("The file mytest.txt deleted successfully.");
+            //Console.WriteLine("File does not exist");
+
+            //3
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (File.Exists(path))
+            //{
+            //    Console.WriteLine("A file created with name mytest.txt ");
+            //}
+            //else
+            //{
+            //    File.Create(path);
+            //}
+
+            //4
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("A file created with content name mytest.txt ");
+            //}
+
+            //5
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Here is the content of the file mytest.txt.Hello and Welcome . It is the first content of the text file mytest.txt.");
+            //}
+
+            //using (StreamReader sr = new StreamReader(path))
+            //{
+            //    string txt = sr.ReadLine();
+            //    Console.WriteLine(txt);
+            //}
+
+            //6
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Hello and Welcome\r\n It is the first content\r\n of the text file mytest.txt.");
+            //}
+
+            //string[] txt = File.ReadAllLines(path);
+
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(txt[x]);
+
+            //7
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Hello and Welcome\r\n It is the first content\r\n of the text file mytest.txt.");
+            //}
+
+            //File.AppendAllText(path, "This is the line appended at last line.");
+
+            //string append = File.ReadAllText(path);
+
+            //Console.WriteLine(append);
+
+            //8
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Hello C#.");
+            //}
+
+            //using (StreamReader sw = new StreamReader(path))
+            //{
+            //    string txt = sw.ReadLine();
+            //    Console.WriteLine(txt);
+            //}
+
+            //File.Copy(path, @"C:\Users\user\Desktop\New folder\mytest1.txt");
+            
+
+            //9
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Hello and Welcome\r\n It is the first content\r\n of the text file mytest.txt.");
+            //}
+
+            //string[] txt = File.ReadAllLines(path);
+
+            //Console.WriteLine(txt[0]);
+
+            //10
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Hello and Welcome\r\n It is the first content\r\n of the text file mytest.txt.");
+            //}
+
+            //string[] txt = File.ReadAllLines(path);
+
+            //Console.WriteLine(txt[txt.Length-1]);
+
+            //11
+            //string path = @"C:\Users\user\Desktop\New folder\mytest.txt";
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            //using (StreamWriter sw = new StreamWriter(path))
+            //{
+            //    sw.WriteLine("Hello and Welcome\r\n It is the first content\r\n of the text file mytest.txt.");
+            //}
+
+            //string[] txt = File.ReadAllLines(path);
+
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(txt[x]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
